5f399f93fbc393d7cc5017119d48a4c702a0d27b
djordje.bajic <djordje.bajic@humanity.com>
2018-11-09 10:28:27 +0100

    Test

--- src/main/java/Authentication/Auth/routes/Oauth2.java
+++ src/main/java/Authentication/Auth/routes/Oauth2.java
@@ -7,22 +7,10 @@ public class Oauth2 extends RouteBuilder {
 	@Override
 	public void configure() throws Exception {
 
-		/*restConfiguration()
-        .component("restlet")
-        .host("localhost").port("8080");
-		
-		rest("/api/customers")
-	    .get().to("direct:nesto")
-	    .post().consumes("application/json").to("direct:nesto2");
-		
-		from("direct:nesto")
-		.setBody().constant("{\"method\":\"GET\"}").to("mock:nesto");
-		
-		from("direct:nesto2")
-		.setBody().constant("{\"method\":\"POST\"}").to("mock:nesto");
-		*/
 		from("timer:foo?period=15000")
 		.log("Started!")
+		.to("https://jsonplaceholder.typicode.com/todos/1")
+		.log("${body}")
 		.to("mock:123321");
 		
 	}
